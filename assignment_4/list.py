f463f8ff418d2379365a0b5facde0d8277d0573c
adrianomucha@gmail.com <AdrianMucha>
2016-07-07 03:05:07 +0000

    added assignment_5

--- assignment_4/list.py
+++ assignment_4/list.py
@@ -4,5 +4,6 @@ dbFilename = "task_database_python.txt"
 dbPath = os.path.join( os.getenv("HOME"), dbFilename )
 with open(dbPath, "r") as myfile:
     dbContents = myfile.read()
-    trimmedContents = dbContents.rstrip()
+    print(trimmedContents) = dbContents.rstrip()
+parsedTask = parseArguments(sys.argv)
 
